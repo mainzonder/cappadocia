3fcdfbf48b10ecfb2e48d7bb66b06ae327a2d8ad
mainzonder <48353743+mainzonder@users.noreply.github.com>
2020-06-17 16:18:20 +0300

    fourth

--- js/main.js
+++ js/main.js
@@ -7,4 +7,12 @@ $(function () {
     autoplay: true,
     fade: true,
   });
+  $(".our-trip__slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+    autoplay: true,
+    fade: true,
+  });
 });
